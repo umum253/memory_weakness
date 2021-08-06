36f64246598e5abb258a1ef9e9bb7fa06ce97377
umum253 <fkwpostpost@gmail.com>
2021-08-06 18:19:37 +0900

    make shuffle function

--- js/memory_weakness.js
+++ js/memory_weakness.js
@@ -1,5 +1,38 @@
 // 52枚（4つの絵柄×13枚）のカードを用意する
+let card = [];
+let type = ["heart", "spade", "clover", "diamond"];
+
+for(let i = 0; i < type.length; i++) {
+    for(let j = 1; j < 14; j++) {
+        card.push("card_" + type[i] + "_" + j + ".png");
+    }
+}
+// console.log("card.length: " + card.length);
+
 // カードをシャッフルする
+let rndCard = [];
+let num = [];
+let chofukuNum = [];
+
+for(i = 0; i < 52; i++) {
+    num.push(i);
+}
+
+console.log("rndCard.length: " + rndCard.length);
+
+while(rndCard.length < 52) {
+    let rndNum = Math.floor(Math.random()*52);
+    if(!chofukuNum.includes(rndNum)) {
+        rndCard.push(card[rndNum]);
+        chofukuNum.push(rndNum);
+    }
+}
+
+console.log("rndCard.length: " + rndCard.length);
+console.log(rndCard);
+
+
+
 // カードを表示させる
 // カードをめくる
 // 1枚目と2枚目の結果を照合する

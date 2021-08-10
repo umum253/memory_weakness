4efd8f352e4c237e5d030506dc257dcb2322a80e
umum253 <fkwpostpost@gmail.com>
2021-08-10 09:52:22 +0900

    feat: カード表示機能

--- js/memory_weakness.js
+++ js/memory_weakness.js
@@ -1,39 +1,53 @@
-// 52枚（4つの絵柄×13枚）のカードを用意する
-let card = [];
-let type = ["heart", "spade", "clover", "diamond"];
+// カードの枚数
+const cardLength = 52;
 
-for(let i = 0; i < type.length; i++) {
-    for(let j = 1; j < 14; j++) {
-        card.push("card_" + type[i] + "_" + j + ".png");
+// 画面読み込み時に実行
+window.onload = function () {
+
+    // 52枚（4つの絵柄×13枚）のカードを用意する
+    let card = [];
+    let type = ["heart", "spade", "clover", "diamond"];
+
+    for(let i = 0; i < type.length; i++) {
+        for(let j = 1; j < 14; j++) {
+            card.push("card_" + type[i] + "_" + j + ".png");
+        }
     }
-}
-// console.log("card.length: " + card.length);
+    // console.log("card.length: " + card.length);
 
-// カードをシャッフルする
-let rndCard = [];
-let num = [];
-let chofukuNum = [];
+    // カードをシャッフルする
+    let rndCard = [];
+    let num = [];
+    let chofukuNum = [];
 
-for(i = 0; i < 52; i++) {
-    num.push(i);
-}
-
-console.log("rndCard.length: " + rndCard.length);
-
-while(rndCard.length < 52) {
-    let rndNum = Math.floor(Math.random()*52);
-    if(!chofukuNum.includes(rndNum)) {
-        rndCard.push(card[rndNum]);
-        chofukuNum.push(rndNum);
+    for(i = 0; i < cardLength; i++) {
+        num.push(i);
     }
+
+    console.log("rndCard.length: " + rndCard.length);
+
+    while(rndCard.length < cardLength) {
+        let rndNum = Math.floor(Math.random()*52);
+        if(!chofukuNum.includes(rndNum)) {
+            rndCard.push(card[rndNum]);
+            chofukuNum.push(rndNum);
+        }
+    }
+
+    console.log(rndCard);
+    console.log("rndCard.length: " + rndCard.length);
+
+    // カードを表示させる
+
+    let allPanel = "";
+    let panel = document.getElementById("panel");
+    for (i = 0; i < cardLength; i++) {
+        allPanel = allPanel + `<p>${rndCard[i]}</p>`;
+    }
+    panel.innerHTML = allPanel;
+
 }
 
-console.log("rndCard.length: " + rndCard.length);
-console.log(rndCard);
-
-
-
-// カードを表示させる
 // カードをめくる
 // 1枚目と2枚目の結果を照合する
 // 　一緒であればカードを非表示にする

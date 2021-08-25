7e7bbbd73964cdec63509b457b7b79052363c757
umum253 <fkwpostpost@gmail.com>
2021-08-25 09:14:41 +0900

    update: 誤記修正、コメント修正

--- js/memory_weakness.js
+++ js/memory_weakness.js
@@ -4,9 +4,9 @@ let playerList = [];
 // プレイヤーごとにそろったペアをカウントするリスト
 let countList = [0, 0, 0];
 // 現在のプレイヤーを判断する変数
-let curPlayerJudgeing = 0;
+let curPlayerJudging = 0;
 // ゲームの終了を判断する変数
-let gameFinishJudgeing = 0;
+let gameFinishJudging = 0;
 // 取得する要素と挿入するタグ
 let playerElm = '';
 let player = 0;
@@ -22,24 +22,25 @@ let resultTag = '';
 let result = '';
 // カードの合計枚数
 const CARD_LENGTH = 52;
-// カードを順番通りに格納するリスト
-let card = [];
 // カードの絵柄
 let type = ["heart", "spade", "club", "diamond"];
+// カードを順番通りに格納するリスト
+let card = [];
 // カードをシャッフルして格納するリスト
 let rndCard = [];
 // カードをシャッフルするとき重複をなくすためのリスト
 let chofukuNum = [];
 // 1枚目のカード
-let FirstCardElm;
+let FirstCardElm = '';
 let firstCardId = '';
 // 2枚目のカード
-let SecondCardElm;
+let SecondCardElm = '';
 let secondCardId = '';
 // 1枚目のカードかどうかのフラグ
 let firstCardFlg = true;
 
-// はずれた場合に裏に戻す処理を遅らせるためのタイマー定義
+// 処理を指定秒数遅らせるタイマー定義
+// (awaitで呼び出すとPromise処理の結果が返ってくるまで一時停止する)
 function delay(n) {
     return new Promise(function(resolve){
         setTimeout(resolve, n*1000);
@@ -49,15 +50,16 @@ function delay(n) {
 // STARTボタンを非活性にしておく
 document.getElementById("start").disabled = true;
 
-// OKボタンクリック時に実行(ゲーム設定)
+// OKボタンクリック時に実行する処理
 document.getElementById("ok").onclick = function () {
+    // 入力されたプレイヤー数を取得する
     playerElm = document.getElementById("player");
     player = playerElm.value
     playerName = document.getElementById("player_name");
-    // 入力されていない場合、アラートを表示する
+    // プレイヤー数が入力されていない場合、アラートを表示する
     if (player == 0) {
         alert("人数を設定してください")
-    // 入力されている場合、人数分の名前入力欄を表示してSTARTボタンを活性化
+    // プレイヤー数が入力されている場合、名前入力欄を表示しSTARTボタンを活性化する
     } else {
         for (let i = 0; i < player; i++) {
             playerNameTag += `${i + 1}人目 <input type="text" id=player_name${i} maxlength="10"/> さん<br>`;
@@ -68,19 +70,19 @@ document.getElementById("ok").onclick = function () {
     }
 }
 
-// STARTボタンクリック時に実行
+// STARTボタンクリック時に実行する処理
 document.getElementById("start").onclick = function () {
-    // 入力された名前をリストに入れる
+    // 入力されたプレイヤーの名前を取得する
     for (let i = 0; i < player; i++) {
         playerList.push(document.getElementById("player_name" + i).value);
     }
 
-    // 現在のプレイヤーを表示
+    // 現在のプレイヤーを表示する
     curPlayer = document.getElementById("current_player");
     curPlayerTag = `${playerList[0]}さんのターンです`;
     curPlayer.textContent = curPlayerTag;
 
-    // プレイヤーごとのカウントを表示
+    // プレイヤーごとのカウントを表示する
     countTag = "";
     count = document.getElementById("count");
     for (let i = 0; i < player; i++) {
@@ -114,7 +116,7 @@ document.getElementById("start").onclick = function () {
     panel.innerHTML = panelTag;
 }
 
-// カードをクリック時に実行
+// カードクリック時に実行する処理
 document.getElementById("panel").addEventListener('click', async function(event) {
     let target = event.target;
     // ゲーム開始時、STARTボタンを非活性にしておく
@@ -140,36 +142,36 @@ document.getElementById("panel").addEventListener('click', async function(event)
             // そろった場合、非表示にする
             if (firstCardId.split('_')[2] == secondCardId.split('_')[2]) {
                 console.log('そろった！');
-                // 1秒待って(awaitはPromise処理の結果が返ってくるまで一時停止してくれる)、非表示にする
+                // 1秒待ってからカードを非表示にする
                 await delay(1);
                 document.getElementById(firstCardId).style.visibility ="hidden";
                 document.getElementById(secondCardId).style.visibility ="hidden";
                 // プレイヤーごとのカウントを変更する
-                countList[curPlayerJudgeing % player]++;
+                countList[curPlayerJudging % player]++;
                 countTag = "";
                 count = document.getElementById("count");
                 for (let i = 0; i < player; i++) {
                     countTag += `${playerList[i]}さん ${countList[i]}ペア　`;
                 }
                 count.textContent = countTag;
-                gameFinishJudgeing++;
+                gameFinishJudging++;
             // はずれた場合、裏に戻す
             } else {
                 console.log('はずれた');
-                // 1秒待って(awaitはPromise処理の結果が返ってくるまで一時停止してくれる)、裏に戻す
+                // 1秒待ってからカードを裏に戻す
                 await delay(1);
                 FirstCardElm.src = './img/back.png';
                 SecondCardElm.src = './img/back.png';
                 // 現在のプレイヤーを変更する
-                curPlayerJudgeing++;
+                curPlayerJudging++;
                 curPlayer = document.getElementById("current_player");
-                curPlayer.textContent = `${playerList[curPlayerJudgeing % player]}さんのターンです`;
+                curPlayer.textContent = `${playerList[curPlayerJudging % player]}さんのターンです`;
             }
         }
     }
 
     // ゲーム終了時、勝った人を表示する
-    if (gameFinishJudgeing == CARD_LENGTH/2) {
+    if (gameFinishJudging == CARD_LENGTH/2) {
         // TODO: 同率1位に対応できてない
         let winner = countList.indexOf(Math.max.apply(null, countList));
         resultTag = "";
